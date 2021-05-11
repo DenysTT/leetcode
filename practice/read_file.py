559cd1f7c30ce601b4e658669f765ca24c3c6ee0
Denys Balastryk <denys.balastryk@zopa.com>
2021-05-11 10:37:56 +0100

    read by chunks

--- practice/read_file.py
+++ practice/read_file.py
@@ -3,10 +3,17 @@ import os
 def print_args(args):
     try:
         with open('text.txt') as file:
-            for line in file:
+            for line in read_by_chunks(file):
                 print(line)
     except Exception as e:
         print(e)
+
+def read_by_chunks(file, chunk = 1024)
+    while True:
+        data = file.read(chunk)
+        if not data:
+             break
+        yield data
 
 if __name__ == "__main__":
     print_args(sys.argv)
